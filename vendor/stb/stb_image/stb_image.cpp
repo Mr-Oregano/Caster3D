ee5f8103a3119a09368ee793494f57209834685e
Spice <kf.shared.ptr@gmail.com>
2022-04-27 19:40:37 -0500

    Added stb_image.h library.

---
+++ vendor/stb/stb_image/stb_image.cpp
@@ -0,0 +1,7 @@
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define __STDC_LIB_EXT1__
+
+#include "stb_image_write.h"
+#include "stb_image.h"
--- vendor/stb/stb_image/stb_image_write.cpp
+++
@@ -1,5 +0,0 @@
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#define __STDC_LIB_EXT1__
-
-#include "stb_image_write.h"
